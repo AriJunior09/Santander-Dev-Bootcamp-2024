00b02421ce61e5d6829abb719efd3e7bd2930876
arijunior09 <arioliveirajuniorjose@gmail.com>
2024-07-22 20:55:03 -0300

    create repository

---
+++ src/test/java/santander_bootcamp/SantanderDevBootcamp2024ApplicationTests.java
@@ -0,0 +1,13 @@
+package santander_bootcamp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SantanderDevBootcamp2024ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
