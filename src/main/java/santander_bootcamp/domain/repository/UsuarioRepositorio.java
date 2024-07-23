ed9613c5f6d631a0da0d935271b8029b80137788
arijunior09 <arioliveirajuniorjose@gmail.com>
2024-07-22 22:06:49 -0300

    create classe UsuarioRepositorio

--- src/main/java/santander_bootcamp/domain/repository/UsuarioRepositorio.java
+++ src/main/java/santander_bootcamp/domain/repository/UsuarioRepositorio.java
@@ -1,7 +1,9 @@
 package santander_bootcamp.domain.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import santander_bootcamp.domain.model.Usuario;
 
+@Repository
 public interface UsuarioRepositorio extends JpaRepository<Usuario, Long>{
 }
